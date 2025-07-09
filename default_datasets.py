670e0bda8e4682822e66961f38583a74e6defd2c
SURIAPRAKASH1 <suriaprakashsuri@gmail.com>
2025-07-09 01:59:47 -0700

    default datasets for training ddpm's

---
+++ default_datasets.py
@@ -0,0 +1,100 @@
+import torch
+import torchvision
+from torch.utils.data import DataLoader, Sampler
+from torch.utils.data.distributed import DistributedSampler
+from torchvision.transforms import v2
+
+from typing import Literal, Iterable, Union
+
+
+def preprocessing_pipeline(image_height: int, image_width: int):
+
+    "Image preprocessing pipeline"
+
+    transform = v2.Compose([
+            v2.ToImage(),                                                     # convert PIL image/array to tensor
+            v2.Resize(size = (image_height, image_width), antialias= True),      # v2.Resize() requires input to be tensor
+            v2.RandomHorizontalFlip(),
+            v2.ToDtype(torch.float32),
+            v2.Lambda(lambda t: (t / 255 ) * 2 - 1)                           # normalize image in range of [-1, 1]
+        ])
+
+    return transform
+
+
+def reverseprocessing_pipeline():
+
+    " reverse pipline for normalized/transformed images to its original format"
+
+    reverse_transform = v2.Compose([
+        v2.ToImage(),
+        v2.Lambda(lambda t: (t + 1)/ 2 * 255),                           # convert to original pixel value
+        v2.Lambda(lambda t: t.permute(1, 2, 0) if t.size(0) > 4 else t ),
+        v2.ToDtype(torch.uint8),                                         # carefull with dtype
+        v2.ToPILImage()                                                  # convert to PIL image
+    ])
+
+    return reverse_transform
+
+
+class PrepareDatasetDataLoader():
+
+    """
+    Preparing Dataset and DataLoader for given dataset_name.
+    """
+    
+    def __init__(self, 
+                dataset_name: Literal["FashionMNIST", "CIFAR10", "CelebA"], 
+                transform: v2.Compose, 
+                batch_size: int, 
+                pin_memory: bool, 
+                world_size: int = 1,
+                rank: int = 0,
+                shuffle: bool = False, 
+                num_workers: int = 0,
+                ):
+
+        self.batch_size = batch_size
+        self.pin_memory = pin_memory
+        self.num_workers = num_workers
+        
+        if dataset_name == "FashionMNIST":
+            # Fashion Mnist dataset
+            fmnist_root = "github.com/zalandoresearch/fashion-mnist/blob/master/data/"
+            self.dataset = torchvision.datasets.FashionMNIST(fmnist_root, train= True, download= True, transform= transform)
+            
+        elif dataset_name == "CIFAR10":
+            # CIFAR10 dataset
+            cifar10_root = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+            self.dataset = torchvision.datasets.CIFAR10(cifar10_root, train= True, download= True, transform= transform)
+
+        elif dataset_name == "CelebA":
+            # CelebA dataset
+            # celebA_root = "https://drive.google.com/drive/folders/0B7EVK8r0v71pWEZsZE9oNnFzTm8?resourcekey=0-5BR16BdXnb8hVj6CNHKzLg&usp=sharing"
+            celebA_root = "https://www.kaggle.com/datasets/jessicali9530/celeba-dataset"
+            self.dataset = torchvision.datasets.CelebA(celebA_root, split = 'train', target_type = 'attr' ,download = True)
+        
+        # if machine have multiple process
+        self.sampler = None
+        if world_size > 1:
+            self.sampler = DistributedSampler(
+                dataset= self.dataset, 
+                num_replicas= world_size, 
+                rank = rank, 
+                shuffle= True, 
+                drop_last= True,
+            )
+
+        self.shuffle = False if self.sampler else shuffle
+
+    def prepare_dataloader(self):
+
+        dataloader = DataLoader(self.dataset, 
+                                batch_size= self.batch_size, 
+                                drop_last= True, 
+                                shuffle= self.shuffle,
+                                pin_memory= self.pin_memory, 
+                                sampler=  self.sampler, 
+                                num_workers= self.num_workers)
+        
+        return dataloader
